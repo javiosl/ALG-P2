d779ac403c1a6de038479663b7bca36d6c5c90bf
María del Mar <marmrobles@correo.ugr.es>
2023-04-05 21:36:36 +0200

    gráficas del avanzado y su fit

--- script-salida.sh
+++ script-salida.sh
@@ -3,5 +3,6 @@ a=2
 for ((P=7500;P<=187500;P=P+7500))
 do 
 B=$(( $P - $a ))
- ./buscandokesimo-sencillo $P $B >> salidaSencillo.dat
+./buscandokesimo-sencillo $P $B >> salidaSencillo.dat
+./buscandokesimo-dyv $P $B >> salidaDyV.dat
 done
